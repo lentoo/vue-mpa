592e16e914066ec81b650d7c3d08eba444433c1b
lentoo <729533020@qq.com>
2018-06-15 11:50:43 +0800

    vue-cli 改造成多页面配置完成

---
+++ src/modules/main/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Main from './main.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#main',
+  components: {
+    Main
+  },
+  template: '<Main/>'
+})
---
+++ src/modules/main/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="main">
+    <img src="@/assets/logo.png">
+    <h2>this is Main module</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+#main {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
